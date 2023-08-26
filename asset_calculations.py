06fef629ca40d8a5004db2b443f30f47fad214c7
jack <jack_greenaway@outlook.com>
2023-08-26 12:39:31 +0100

    added drawdown function

--- asset_calculations.py
+++ asset_calculations.py
@@ -1,4 +1,6 @@
 import numpy as np
+import yfinance as yf
+import matplotlib.pyplot as plt
 
 def sharpe_ratio(asset_returns: float, rfr: float): 
     """
@@ -30,6 +32,44 @@ def beta(index_returns: float, asset_returns: float):
     
     covariance = np.cov(index_returns, asset_returns)[0][1]
     return covariance / np.var(index_returns)
+
+
+
+def asset_drawdown(asset: str, start_time: str, end_time: str=None, plot: bool=False):
+    """
+    returns drawdown information for a given asset
+    
+    parameters:
+        asset: asset ticker
+        start_time: time to start analysis from
+        end_time: optional time to end analysis data
+        plot: optional bool to plot the information as opposed to receiving values
+    returns:
+        daily_drawdown (float, vector): vector of daily drawdowns
+        max_daily_drawdowns (float, vector): vector of the maximum daily drawdown over a 252 day window 
+    """
+    
+    data = yf.download(asset, start=start_time, end=end_time)["Adj Close"]
+    window = 252
+    
+    rolling_max = data.rolling(window, min_periods=1).max()
+    daily_drawdown = data / rolling_max - 1
+    max_daily_drawdown = daily_drawdown.rolling(window, min_periods=1).min()
+    
+    if plot:
+        fig, ax = plt.subplots(2, 1, figsize=(18, 6), sharex=False)
+
+        ax[0].plot(daily_drawdown * 100, label="Daily Drawdown")
+        ax[0].plot(max_daily_drawdown * 100, label="Max Daily Drawdown")
+        ax[0].set_ylabel("% Change")
+        ax[0].legend()
+
+        ax[1].plot(data, label="Stock Price")
+        ax[1].set_ylabel("Stock Price ($)")
+        ax[1].legend()
+    else:
+        return daily_drawdown, max_daily_drawdown
+    
     
     
     
